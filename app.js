a39ebbbfb6cf7ba44bf8c02e45fc97b89c936216
bilashcse <bilashcse@gmail.com>
2018-07-24 19:09:40 +0600

    algorithm update

--- app.js
+++ app.js
@@ -7,13 +7,12 @@ var misc = require('./replace.js');
 
 function goToTemplateEngine(data, htmlUrl) {
 
-    var htmlTemplate = fs.readFileSync(path.join(__dirname, htmlUrl), 'utf8');
-
-    var re = /<script\b[^>]*>([\s\S]*?)<\/script>/gm;
+    var htmlTemplate = htmlUrl ? fs.readFileSync(path.join(__dirname, htmlUrl), 'utf8') : html ;
+    var reg = /<script\b[^>]*>([\s\S]*?)<\/script>/gm;
     var match, arrValues = [], templateMap = {};
 
-    for (var key in data) {
-        if ((typeof data[key]) == 'object') {
+     for (var key in data) {
+        if (Array.isArray(data[key])) {
             arrValues.push({
                 key: key,
                 values: data[key]
@@ -21,14 +20,14 @@ function goToTemplateEngine(data, htmlUrl) {
         }
     }
 
-    while (match = re.exec(htmlTemplate)) {
+   while (match = reg.exec(htmlTemplate)) {
         var templateName = (((/({{.*}})/).exec(match[1]))[0].replace(/{{|}}/g, '')).split(".")[0];
         templateMap[templateName] = match[1];
 
     }
 
     arrValues.forEach(function(val) {
-        var templateHtml = templateMap[val.key].trim();
+        var templateHtml = templateMap[val.key];
         var updatedHtml = '';
         
         val.values.forEach(function(data) {
@@ -41,8 +40,6 @@ function goToTemplateEngine(data, htmlUrl) {
 
         data[val.key] = updatedHtml;
     });
-
-
 
     return misc.replaceKeysInHtml(htmlTemplate, data);
 }
